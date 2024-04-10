1e133e663d0dcd7a7ba0b2ff555c2011f1f69061
Gradyn Nagle <gradyndnagle@outlook.com>
2024-04-09 18:35:38 -0700

    added initial sections and site layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,28 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang='ts'>
+    let scrollTop = 0;
+    function handleScroll(event: UIEvent) {
+      scrollTop = (event.target as Element).scrollTop;
+    }
+</script>
+
+<style lang='scss'>
+.child {
+    height: 100vh;
+    width: 100%;
+    scroll-snap-align: start;
+    margin: 0;
+    box-sizing: border-box;
+}
+</style>
+
+<div>
+    <section class="child">
+        Section 1
+    </section>
+    <section class="child">
+        Section 2
+    </section>
+    <section class="child">
+        Section 3
+    </section>
+</div>
